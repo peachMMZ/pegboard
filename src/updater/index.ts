2d63e1050bde7c4f4eef4036db0e84361a8ba630
MaYu <mayudada@qq.com>
2025-08-04 16:07:01 +0800

    feat(updater): 添加应用更新检查及安装功能

---
+++ src/updater/index.ts
@@ -0,0 +1,56 @@
+import { h } from 'vue'
+import { NCard, NTag, createDiscreteApi } from 'naive-ui'
+import { check, Update } from '@tauri-apps/plugin-updater'
+import { useThemeStore } from '@/store/theme'
+
+const updateNoticeContent = (update: Update) => {
+  return h('div', [
+    h(
+      NCard,
+      {
+        title: '更新内容',
+        size: 'small',
+        headerExtra: () => h(NTag, { type: 'primary', size: 'tiny' }, { default: () => `v${update.version}` }),
+        embedded: true
+      },
+      { default: () => update.body }
+    )
+  ])
+}
+
+export async function checkUpdate() {
+  const update = await check()
+  if (update) {
+    const themeStore = useThemeStore()
+    const { dialog } = createDiscreteApi(['dialog'], { configProviderProps: { themeOverrides: themeStore.themeOverride } })
+    const dialogRef = dialog.create({
+      title: '发现新版本( •̀ ω •́ )✧',
+      content: () => updateNoticeContent(update),
+      draggable: true,
+      positiveText: '立即更新',
+      onPositiveClick: async () => {
+        dialogRef.loading = true
+        dialogRef.negativeText = undefined
+        dialogRef.closable = false
+        dialogRef.maskClosable = false
+        let downloaded = 0
+        let contentLength = 0
+        await update.downloadAndInstall((e) => {
+          switch(e.event) {
+            case 'Started':
+              contentLength = e.data.contentLength || 0
+              break
+            case 'Progress':
+              downloaded += e.data.chunkLength
+              const percent = Math.round((downloaded / contentLength) * 100)
+              dialogRef.positiveText = `${percent}%`
+              break
+            case 'Finished':
+              break
+          }
+        })
+      },
+      negativeText: '稍后再说'
+    })
+  }
+}
